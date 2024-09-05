06467daefe2657daa1c1f0b34c1e12fafbd25913
Manmath Hatte <159113658+CodeSniperMan@users.noreply.github.com>
2024-09-05 21:28:47 +0530

    Update anagram.js
    manmath

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,21 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.size() != str2.size())    return flase;
+
+  let charFreq = {};
+  for(let char of str1) {
+    charFreq[char] = charFreq[char] + 1;
+  }
+
+  for(let char of str2) {
+    if(!charFreq[char])
+      return false;
+    charFreq[char]--;
+  }
+
+
+  return true;
 
 }
 
